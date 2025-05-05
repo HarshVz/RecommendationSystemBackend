ba74502d6f20a333eabb8d748622f0b9d19edd36
Harshvz <harshvisualz@gmail.com>
2025-05-05 13:33:26 +0530

    renderv4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
+from flask import Flask, request, jsonify
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from scipy.spatial import distance
